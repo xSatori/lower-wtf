3b759fa44a505ea72e3e30b60eed950ad904fc29
xSatori <99294685+xSatori@users.noreply.github.com>
2024-03-21 12:06:56 -0400

    lower

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
-const lowerRed = '#018A08'
+const lowerRed = '#8a0303'
 import Link from 'next/link'
 
 function animationDelay(i: number){
@@ -84,7 +84,7 @@ export default function Home() {
   const numArrows = 40
 
   return (
-    <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-[#018A08] text-white">
+    <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-[#8a0303] text-white">
       <div className="text-6xl font-medium z-10">
         lower.
       </div>
@@ -102,9 +102,9 @@ export default function Home() {
         <Link className='group cursor-pointer' href={'https://warpcast.com/~/channel/lower'} target='_blank'>
         <svg width="40" height="40" viewBox="0 0 1000 1000" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect className='fill-white opacity-45 group-hover:opacity-80 ease-in-out'  width="1000" height="1000" rx="200" />
-          <path className='fill-[#018A08]' d="M257.778 155.556H742.222V844.444H671.111V528.889H670.414C662.554 441.677 589.258 373.333 500 373.333C410.742 373.333 337.446 441.677 329.586 528.889H328.889V844.444H257.778V155.556Z"/>
-          <path className='fill-[#018A08]' d="M128.889 253.333L157.778 351.111H182.222V746.667C169.949 746.667 160 756.616 160 768.889V795.556H155.556C143.283 795.556 133.333 805.505 133.333 817.778V844.444H382.222V817.778C382.222 805.505 372.273 795.556 360 795.556H355.556V768.889C355.556 756.616 345.606 746.667 333.333 746.667H306.667V253.333H128.889Z"  />
-          <path className='fill-[#018A08]' d="M675.556 746.667C663.283 746.667 653.333 756.616 653.333 768.889V795.556H648.889C636.616 795.556 626.667 805.505 626.667 817.778V844.444H875.556V817.778C875.556 805.505 865.606 795.556 853.333 795.556H848.889V768.889C848.889 756.616 838.94 746.667 826.667 746.667V351.111H851.111L880 253.333H702.222V746.667H675.556Z"  />
+          <path className='fill-[#8a0303]' d="M257.778 155.556H742.222V844.444H671.111V528.889H670.414C662.554 441.677 589.258 373.333 500 373.333C410.742 373.333 337.446 441.677 329.586 528.889H328.889V844.444H257.778V155.556Z"/>
+          <path className='fill-[#8a0303]' d="M128.889 253.333L157.778 351.111H182.222V746.667C169.949 746.667 160 756.616 160 768.889V795.556H155.556C143.283 795.556 133.333 805.505 133.333 817.778V844.444H382.222V817.778C382.222 805.505 372.273 795.556 360 795.556H355.556V768.889C355.556 756.616 345.606 746.667 333.333 746.667H306.667V253.333H128.889Z"  />
+          <path className='fill-[#8a0303]' d="M675.556 746.667C663.283 746.667 653.333 756.616 653.333 768.889V795.556H648.889C636.616 795.556 626.667 805.505 626.667 817.778V844.444H875.556V817.778C875.556 805.505 865.606 795.556 853.333 795.556H848.889V768.889C848.889 756.616 838.94 746.667 826.667 746.667V351.111H851.111L880 253.333H702.222V746.667H675.556Z"  />
         </svg>
         </Link>
 
@@ -131,7 +131,7 @@ export default function Home() {
       <div className="arrow-container z-0">
         {
           [...Array(numArrows).keys()].map((i) => (
-            <div key={i.toString()} className="arrow text-[#48ad4d]" style={{left: `${i / numArrows * 100}%`, animationDelay: `${animationDelay(i).toFixed(2)}s`}}>
+            <div key={i.toString()} className="arrow text-[#d45c5c]" style={{left: `${i / numArrows * 100}%`, animationDelay: `${animationDelay(i).toFixed(2)}s`}}>
               ↓
             </div>
           ))
