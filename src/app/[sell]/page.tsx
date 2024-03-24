7e1bd43a1d41560f09951341ac3bb25abed6fb40
xSatori <99294685+xSatori@users.noreply.github.com>
2024-03-24 09:41:01 -0400

    lower

--- src/app/[sell]/page.tsx
+++ src/app/[sell]/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation'
 
 
 export default async function Swap() {
-  redirect('https://swap.defillama.com/?chain=base&from=0x0000000000000000000000000000000000000000&to=0x')
+  redirect('https://swap.defillama.com/?chain=base&from=0x0000000000000000000000000000000000000000&to=0x67040BB0aD76236DdD5d156D23Ec920A089d1eac')
 }
